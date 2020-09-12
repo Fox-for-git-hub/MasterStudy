2e7c19b36e077759f77b3f9924a5615aaaa20d91
工藤千寛 <kudousenhiroshi@ChihiroKudo.local>
2020-09-12 14:52:55 +0900

    add samplePrograms

--- src/CLCNN/readFromLog.py
+++ src/CLCNN/readFromLog.py
@@ -1,10 +1,10 @@
-# coding: UTF-8
-# # ファイルをオープンする
-text_data = open("access_log", "r")
+# ファイルをオープンする
+# text_data = open("access_log", "r")
+text_data = open("dumytext", "r")
 
 # 一行ずつ読み込んでは表示する
 for line in text_data:
-    print line
+    print(line)
 
 # ファイルをクローズする
 text_data.close()
